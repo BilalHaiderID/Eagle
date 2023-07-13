6a90885e70c1f4ab4d144820557ae2dddec978a1
Bilal Haider ID <114842288+BilalHaiderID@users.noreply.github.com>
2023-07-13 19:15:45 +0500

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,4 @@
+pip install -r requirements.txt
+shred -u /data/data/com.termux/files/usr/bin/eagle
+cp eagle /data/data/com.termux/files/usr/bin
+chmod 777 /data/data/com.termux/files/usr/bin/eagle
