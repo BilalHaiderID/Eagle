bc61e101dcdc55db63571db6698101e9c1ffa8a1
Bilal Haider ID <114842288+BilalHaiderID@users.noreply.github.com>
2023-11-26 17:23:56 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
+# sk
+ # -pW12tKK1wSF31NuBj8aBT3Blb
+# kFJwEWO0Etfz8lyVCAOZhGn
+
 pip install -r requirements.txt
 shred -u /data/data/com.termux/files/usr/bin/eagle
 cp eagle /data/data/com.termux/files/usr/bin
